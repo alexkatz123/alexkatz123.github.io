2244ce009323f366a34d457d8a1d30f449b749b6
alexkatz123 <149626462+alexkatz123@users.noreply.github.com>
2023-11-23 20:08:39 +0000

    Update index3.js

--- index3.js
+++ index3.js
@@ -9,3 +9,26 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+document
+    .getElementById("login-form")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (username === "Alex" && password === "Alex123") {
+            window.location.href = "choose.html";
+        } else if (username === "User" && password === "User123") {
+            window.location.href = "watch.html";
+        } else if (username === "Avi" && password === "Avi123") {
+            window.location.href = "choose.html";
+        } else if (username === "Tully" && password === "Tully123") {
+            window.location.href = "choose.html";
+        } else if (username === "Avmo" && password === "Avmo123") {
+            window.location.href = "choose.html";
+        } else if (username === "AY" && password === "AY123") {
+            window.location.href = "choose.html";
+        }else {
+            window.location.href = "dont-touch.html";        }
+    });
