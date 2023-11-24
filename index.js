d8489994c26dde53593cd4a78ed4d64cdf5e73e2
alexa2549 <katzalexander0@gmail.com>
2023-11-24 14:33:34 +0000

    appwrite

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
-document
-    .getElementById("login-form")
-    .addEventListener("submit", function (event) {
-        event.preventDefault();
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('Register');
+const loginBtn = document.getElementById('login');
 
-        if (username === "Alex" && password === "Alex123") {
-            window.location.href = "choose.html";
-        } else if (username === "User" && password === "User123") {
-            window.location.href = "watch.html";
-        } else if (username === "Avi" && password === "Avi123") {
-            window.location.href = "choose.html";
-        } else if (username === "Tully" && password === "Tully123") {
-            window.location.href = "choose.html";
-        } else if (username === "Avmo" && password === "Avmo123") {
-            window.location.href = "choose.html";
-        } else if (username === "AY" && password === "AY123") {
-            window.location.href = "choose.html";
-        }else {
-            window.location.href = "dont-touch.html";        }
-    });
-
-const acceptCookiesButton = document.getElementById("accept-cookies");
-const declineCookiesButton = document.getElementById("decline-cookies");
-const cookieConsent = document.getElementById("cookie-consent");
-const overlay = document.createElement("div");
-overlay.classList.add("overlay");
-
-acceptCookiesButton.addEventListener("click", function () {
-    cookieConsent.style.display = "none"; 
-    overlay.style.display = "none"; 
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
 });
 
-declineCookiesButton.addEventListener("click", function () {
-    cookieConsent.style.display = "none";
-    overlay.style.display = "none"; 
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
 });
 
-overlay.style.display = "block";
-document.getElementById("content-wrapper").appendChild(overlay);
+const client = new Client();
+
+client
+    .setEndpoint('https://api.fnukapps.com/v1')
+    .setProject('6560b16b7a620dd1bfaa');
